25680f38ce1690d873d3a9d336f1944937779548
minuminu123 <alsdn96321@gmail.com>
2023-03-29 12:32:53 +0900

    성격이 비슷한 항목들 그룹화, 질문-답변 기능 추가

--- src/main/java/com/example/demo/global/security/service/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/global/security/service/CustomUserDetailsService.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
