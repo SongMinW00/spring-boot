5d58e54c09f9c8aa70a3e078e63eb8b7060db81b
minuminu123 <alsdn96321@gmail.com>
2023-04-01 03:43:47 +0900

    검색 기능 추가

--- src/main/java/com/example/demo/domain/dao/question/QuestionRepository.java
+++ src/main/java/com/example/demo/domain/dao/question/QuestionRepository.java
@@ -4,8 +4,10 @@ import com.example.demo.domain.entity.answer.Answer;
 import com.example.demo.domain.entity.question.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +17,6 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     Question findByTitleAndBody(String title, String body);
     List<Question> findByTitleLike(String title);
     Page<Question> findAll(Pageable pageable);
+    @Query("select distinct q from Question q left outer join Member u1 on q.author=u1 left outer join Answer a on a.question=q left outer join Member u2 on a.author=u2 where q.title like %:kw% or q.body like %:kw% or u1.username like %:kw% or a.body like %:kw% or u2.username like %:kw% ")
+    Page<Question> findAllByKeyword(@Param("kw") String kw, Pageable pageable);
 }
